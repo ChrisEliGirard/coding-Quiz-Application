fac14ccdfad11ccb97b974819a886b54fe8ba366
Chris Girard <chrisegirard@gmail.com>
2023-04-13 05:07:06 -0400

    ReTyped questions to add variety instead of them all being the same

--- Assets/scripts/questionList.js
+++ Assets/scripts/questionList.js
@@ -5,9 +5,34 @@
     "solution": "An Event Handler"
   },
   {
-    "question": "A procedure that is meant to handle events is?",
-    "answers": ["An Event Handler","A Method","The Event Method","The Event Procedure Function"],
-    "solution": "An Event Handler"
+    "question": "Every valid web page can be represented as a tree. This tree is referred to as the?",
+    "answers": ["JavaScript","API","DOM","MOB"],
+    "solution": "DOM"
+  },
+  {
+    "question": "JavaScript uses what kind of interface to access the DOM structure?",
+    "answers": ["An Event Handler","A Method","an API","HTML5"],
+    "solution": "HTML5"
+  },
+  {
+    "question": "Which of the following is not a valid method for generating output to the screen?",
+    "answers": ["document.write","prompt","alert","print"],
+    "solution": "print"
+  },
+  {
+    "question": "Which of these options does NOT require the use of parentheses?",
+    "answers": ["document.write","prompt","innerHTML","console.log"],
+    "solution": "innerHTML"
+  },
+  {
+    "question": "Which of the following does not generate output directly to the screen?",
+    "answers": ["document.write(message)","console.log(message);","element.innerHTML = message;","element.inserAdjecentHTML(position, message)"],
+    "solution": "element.innerHTML = message;"
+  },
+  {
+    "question": "How does prompt differ from alert?",
+    "answers": ["Only alert uses parentheses","The alert will return a value, prompt does not","Only prompt uses parentheses","The prompt will return a value, alert does not"],
+    "solution": "The prompt will return a value, alert does not"
   },
   {
     "question": "A procedure that is meant to handle events is?",
@@ -15,38 +40,13 @@
     "solution": "An Event Handler"
   },
   {
-    "question": "A procedure that is meant to handle events is?",
-    "answers": ["An Event Handler","A Method","The Event Method","The Event Procedure Function"],
-    "solution": "An Event Handler"
+    "question": "In JavaScript, what keyword was first used to declare a variable?",
+    "answers": ["var","variable","The","Is"],
+    "solution": "var"
   },
   {
-    "question": "A procedure that is meant to handle events is?",
-    "answers": ["An Event Handler","A Method","The Event Method","The Event Procedure Function"],
-    "solution": "An Event Handler"
-  },
-  {
-    "question": "A procedure that is meant to handle events is?",
-    "answers": ["An Event Handler","A Method","The Event Method","The Event Procedure Function"],
-    "solution": "An Event Handler"
-  },
-  {
-    "question": "A procedure that is meant to handle events is?",
-    "answers": ["An Event Handler","A Method","The Event Method","The Event Procedure Function"],
-    "solution": "An Event Handler"
-  },
-  {
-    "question": "A procedure that is meant to handle events is?",
-    "answers": ["An Event Handler","A Method","The Event Method","The Event Procedure Function"],
-    "solution": "An Event Handler"
-  },
-  {
-    "question": "A procedure that is meant to handle events is?",
-    "answers": ["An Event Handler","A Method","The Event Method","The Event Procedure Function"],
-    "solution": "An Event Handler"
-  },
-  {
-    "question": "A procedure that is meant to handle events is?",
-    "answers": ["An Event Handler","A Method","The Event Method","The Event Procedure Function"],
-    "solution": "An Event Handler"
+    "question": "What does it mean that variables are case-sensitive?",
+    "answers": ["That all variables must use uppercase letters","That the computer does not think that the variables name and Name are the same thing","That all variables must use lowercase letters","That every variable has its case(situation)"],
+    "solution": "That the computer does not think that the variables name and Name are the same thing"
   }
 ]
